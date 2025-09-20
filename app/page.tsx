d1a3af683689f605e60719f290741f02bfc3ddae
Ved Gupta <vedgupta@protonmail.com>
2025-09-21 01:11:15 +0530

    feat: update blog metadata generation and improve footer styling; remove outdated blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,10 @@ export default function Page() {
         Ved Prakash Gupta
       </h1>
       <p className="mb-4">
-        Student and Tech enthusiast Developer
+        Full Stack Developer | React Developer | Generative AI Developer | Azure Cloud Certified | Google Cloud Certified | Python Developer
       </p>
       <p className="mb-4">
-        {`It's my passion to write code and solve problems as a Software
-        Developer. Love to help Developers. Always Open for Contributions`}
+        {`I'm a Cloud Engineer with a background in web development and experience in React, Next.js, JavaScript, and TypeScript. I began as a freelancer, then transitioned to a Cloud Developer role at LTIMindtree, where I build scalable cloud solutions and explore ML/DL models. My blend of cloud engineering and development expertise helps me deliver efficient, optimized applications. Connect with me here or at vedgupta0401@gmail.com`}
       </p>
       <div className="my-8">
         <h2 className="mb-8 text-xl font-semibold tracking-tighter">
