fd702a7706d54fc1af406c2e0d2262d7fb998b94
Ved Gupta <vedgupta@protonmail.com>
2025-09-21 02:49:43 +0530

    feat: Update project page description and link to GitHub profile

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,15 +9,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Projects</h1>
-      <p className="text-neutral-600 dark:text-neutral-300 mb-8">
-        I've created a variety of small, useful projects. You can explore them on my{' '}
+      <p className="text-neutral-600 dark:text-neutral-300 mb-8 text-sm">
+        I've created a variety of small, useful projects. You can explore them on my Github{' '}
         <a
           href="https://github.com/innovatorved"
           target="_blank"
           rel="noopener noreferrer"
           className="underline hover:text-neutral-800 dark:hover:text-neutral-100"
         >
-          GitHub profile
+          @innovatorved
         </a>
         .
       </p>
