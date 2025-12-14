06b31a1c59444d0f9df8f3c73ea4807217aeef71
Ved Gupta <vedgupta@protonmail.com>
2025-12-15 01:33:37 +0530

    style: Enhance layout for developer introduction section

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,12 @@ export default function Page() {
       <p className="mb-4">
         Full Stack Developer | React Developer | Generative AI Developer | Azure Cloud Certified | Google Cloud Certified | Python Developer
       </p>
-      <p className="mb-4">
+      <div className="mb-4 rounded-sm border-neutral-200 bg-neutral-50 px-4 py-3 text-sm text-neutral-700 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-300">
+        <p className="m-0">
+          Just trying to be a developer while looking for the right role. If you know of any vacancies, feel free to let me know.
+        </p>
+      </div>
+      <p className="mb-4 text-sm">
         {`Cloud Developer with a strong background in cloud solutions and web development. After starting as a freelancer, I gained expertise in React, Next.js, JavaScript, TypeScript, Docker, Kubernetes, DevOps. Currently, I work at LTIMindtree, where I focus on designing and implementing scalable cloud infrastructure. I am passionate about open-source contributions and enjoy creating projects to explore and innovate with new technologies. Connect with me here or at vedgupta0401@gmail.com`}
       </p>
       <Experience />
